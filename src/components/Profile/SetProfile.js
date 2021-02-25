a3abec1f55e666f6dcd76b08abb1a9c2eba93a3e
Takaaki <dacchin.h081229@gmail.com>
2021-02-25 23:40:30 +0900

    commit

--- src/components/Profile/SetProfile.js
+++ src/components/Profile/SetProfile.js
@@ -1,6 +1,86 @@
 import React, { Component } from "react"
+import firebase from "../../firebase-setup"
 
 class SetProfile extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      image: "",
+      imgURL: "",
+      photoURL: "",
+      defaultPhotoURL: "",
+      errorMessage: "",
+      errorFlag: false,
+    }
+    if (props["image"]) this.state["image"] = props["image"]
+  }
+
+  onInputUsername(e) {
+    const username = e.target.value
+    this.setState({ username })
+    this.handleNameError(username)
+  }
+
+  onSelectThumbnail(e) {
+    if (this.state.image) window.URL.revokeObjectURL(this.state.image)
+    const image = e.target.files[0]
+    if (image) {
+      const imgURL = window.URL.createObjectURL(image)
+      this.setState({ imgURL, image })
+    }
+  }
+
+  nameValidation(username) {
+    return !!username && username.length < 8
+  }
+
+  onClickSetProfileBtn(e) {
+    e.preventDefault()
+    if (this.state.errorMessage) {
+      if (this.state.imgURL) {
+        this.setPhotoURL().then((url) => {
+          this.state.photoURL = url
+          this.updateProfileTask()
+          window.URL.revokeObjectURL(this.state.imgURL)
+        })
+      } else {
+        this.updateProfileTask()
+      }
+    } else {
+      this.handleNameError(this.state.username)
+      this.setState({ errorFlag: true })
+    }
+  }
+
+  setPhotoURL () {
+    const image = this.state.image
+    const imageNameArray = image.name.split('.')
+    const fileType = imageNameArray.pop()
+    const saveImageName = `profilePhoto.${fileType}`
+    const currentUserId = firebase.auth().currentUser.uid
+    const metaData = {
+      contentType: `image/${fileType}`
+    }
+    const photoRef = `images/${currentUserId}/${saveImageName}`
+      const storageRef = firebase.storage().ref(photoRef)
+      return storageRef.put(image, metaData).then(retVal => {
+        return retVal.ref.getDownloadURL()
+      })
+  }
+
+  updateProfileTask() {
+    console.log('update Profile!')
+  }
+
+  handleNameError(username) {
+    let errorMessage = ""
+    if (!username) errorMessage = "入力してください"
+    else if (username.length > 8)
+      errorMessage = "8文字以内で入力してください"
+    this.setState({ errorMessage })
+  }
+
   render() {
     return (
       <div className="container">
@@ -13,10 +93,17 @@ class SetProfile extends Component {
             <label className="form-label" htmlFor="name">
               名前
             </label>
-            <span className="text-danger font-weight-bold">
-              入力してください
-            </span>
-            <input className="form-control" id="name" type="text" />
+            {this.state.errorMessage && this.state.errorFlag && (
+              <span className="text-danger font-weight-bold">
+                {this.state.errorMessage}
+              </span>
+            )}
+            <input
+              className="form-control"
+              id="name"
+              type="text"
+              onInput={this.onInputUsername.bind(this)}
+            />
           </div>
           <div className="form-group form-wrapper">
             <div>
@@ -41,11 +128,15 @@ class SetProfile extends Component {
                 accept="image/*"
                 id="photoURL"
                 style={{ display: "none" }}
+                onChange={this.onSelectThumbnail.bind(this)}
               />
               <p className="img-wrapper">
                 <span className="img-wrapper">
-                  <img src="../../images/default.png" alt="サムネイル" />
-                  {/* <img v-else-if="originPhotoURL" v-bind:src="originPhotoURL" > */}
+                {
+                  this.state.imgURL ?
+                    <img src={this.state.imgURL} /> :
+                    <img src="../../images/default.png" alt="サムネイル" />
+                }
                   <span className="reset-btn">取り消し</span>
                 </span>
                 {/* <img src="@/assets/images/default.png" width="100px" height="100px" /> */}
@@ -53,7 +144,12 @@ class SetProfile extends Component {
             </div>
           </div>
           <div className="btn-wrapper">
-            <button className="btn btn-light border-dark border profile-register-btn">プロフィール設定</button>
+            <button
+              onClick={this.onClickSetProfileBtn.bind(this)}
+              className="btn btn-light border-dark border profile-register-btn"
+            >
+              プロフィール設定
+            </button>
           </div>
         </form>
       </div>
